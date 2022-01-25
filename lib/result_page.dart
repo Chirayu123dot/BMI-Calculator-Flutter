22ca6349906082705de40326c3aaae7f3bec4066
Chirayu Sharma <thechirayu02@gmail.com>
2022-01-25 14:51:52 +0530

    add: skeleton of result page layout

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,4 +1,6 @@
+import 'package:bmi_calculator/resuable_card.dart';
 import 'package:flutter/material.dart';
+import 'constants.dart';
 
 class ResultPage extends StatelessWidget {
   const ResultPage({Key? key}) : super(key: key);
@@ -9,8 +11,43 @@ class ResultPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('hello'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
+            child: Text(
+              'Your Result',
+              style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                    fontSize: 40.0,
+                  ),
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              colour: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [],
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Container(
+              child: Center(
+                child: Text('RE-CALCULATE', style: kButtonTextStyle),
+              ),
+              color: kButtonColor,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: kButtonHeight,
+            ),
+          )
+        ],
       ),
     );
   }
